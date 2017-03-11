6d2bace94e89443a98ad346ac438cb753d2e67b3
destino92 <destinodello@gmail.com>
2017-03-12 01:31:07 +0300

    feat: use request to make an API call to google geolocation

---
+++ app.js
@@ -0,0 +1,8 @@
+const request = require('request');
+
+request({
+  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=united%20state%20international%20university',
+  json: true
+}, (error, response, body) => {
+  console.log(body);
+});
