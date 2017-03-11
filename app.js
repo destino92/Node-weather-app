e5d2f37f32d5d2919ec0fff14c8f554d8a3ea546
destino92 <destinodello@gmail.com>
2017-03-12 01:34:49 +0300

    fix: pretty print object returned

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=united%20state%20international%20university',
   json: true
 }, (error, response, body) => {
-  console.log(body);
+  console.log(JSON.stringify(body, undefined, 2));
 });
