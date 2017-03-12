ffac0da04cc6800c33b1e0580efa101afbfb0f38
destino92 <destinodello@gmail.com>
2017-03-12 15:13:22 +0300

    feat: handle callback errors when making requests to google api

--- app.js
+++ app.js
@@ -20,7 +20,14 @@ request({
   url: `https://maps.googleapis.com/maps/api/geocode/json?address=${address}`,
   json: true
 }, (error, response, body) => {
-  console.log(`Adress: ${body.results[0].formatted_address}`);
-  console.log(`Lat: ${body.results[0].geometry.location.lat}` );
-  console.log(`Lng: ${body.results[0].geometry.location.lng}` );
+  if(error) {
+    console.log('Unable to connect to Google servers.');
+  } else if (body.status === 'ZERO_RESULTS') {
+    console.log('Unable to find that address');
+  } else if (body.status === 'OK') {
+    console.log(`Adress: ${body.results[0].formatted_address}`);
+    console.log(`Lat: ${body.results[0].geometry.location.lat}` );
+    console.log(`Lng: ${body.results[0].geometry.location.lng}` );
+  }
+
 });
