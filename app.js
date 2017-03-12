5e592d4974b65403c74603c36a0809ba3e98dba9
destino92 <destinodello@gmail.com>
2017-03-12 14:43:59 +0300

    fix: use yargs to get user address and encode it before sending request

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+var address = encodeURIComponent(argv.address);
 
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=united%20state%20international%20university',
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${address}`,
   json: true
 }, (error, response, body) => {
-  console.log(JSON.stringify(body, undefined, 2));
+  console.log(`Adress: ${body.results[0].formatted_address}`);
+  console.log(`Lat: ${body.results[0].geometry.location.lat}` );
+  console.log(`Lng: ${body.results[0].geometry.location.lng}` );
 });
